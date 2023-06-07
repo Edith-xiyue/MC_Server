05dd8494b61a42bb022ef0f7f396ae7bed08aaa0
luo_jiahao <2426724347@qq.com>
2023-06-07 16:20:07 +0000

    20230607

--- mc_install.sh
+++ mc_install.sh
@@ -2,11 +2,9 @@ sudo apt-get install wget -y
 sudo apt-get install git -y
 sudo apt-get install vim -y
 sudo apt-get install unzip -y
+unzip bedrock-server-1.19.83.01.zip -D ServerFile
 sudo apt-get install libcurl4-openssl-dev
-tar -zxvf ServerFile.tar.gz -C ServerFile
-git config user.name "luo_jiahao"
-git config user.email "2426724347@qq.com"
-ssh-keygen -t rsa -C "2426724347@qq.com"
-cd ServerFile
+tar -zxvf ServerFile.tar.gz
 sudo ufw allow 19132
+cd ServerFile
 ./bedrock_server
--- mc_upload.sh
+++ mc_upload.sh
@@ -1,4 +1,7 @@
 git remote set-url origin git@github.com:Edith-xiyue/MC_Server.git
+git config user.name "luo_jiahao"
+git config user.email "2426724347@qq.com"
+ssh-keygen -t rsa -C "2426724347@qq.com"
 tar -zcvf ServerFile.tar.gz ServerFile
 git add .
 git commit -m $(date +%Y%m%d)
